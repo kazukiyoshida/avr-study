5c9969419aec35d3c73bc3544dcce97e7e023757
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2019-08-16 17:19:34 +0900

    2-1-2 prescaling

--- section2/2-1-2/main.c
+++ section2/2-1-2/main.c
@@ -5,12 +5,18 @@
 
 int main(void) {
   unsigned long cnt = 0;  // 32bit
+  unsigned long p = 0;
+
   DDRC = 0x30;  // PC5/4 を出力ピンに設定
   PORTC = 0x30; // PC5/4 の出力をH/Hに設定
 
   for (;;) {
     wdt_reset();
-    cnt++;
+    p++;
+    if (p==4) {
+      p = 0;
+      cnt++;
+    }
     if (cnt >= CTOP) {
       cnt = 0;
       PORTC ^= 0x10;
